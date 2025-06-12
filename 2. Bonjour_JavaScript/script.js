4feeec7913f36a2b94c88e42849b0301a734d423
Elouan <elouangauriaud@gmail.com>
2025-06-12 14:30:05 +0200

    Etape 4

--- 2. Bonjour_JavaScript/script.js
+++ 2. Bonjour_JavaScript/script.js
@@ -1,10 +1,18 @@
-function sayHello(firstname) {
+function sayHello(firstname, hour) {
 
 // let firstname = "Beyonce";
 
 let message = `Bonjour ${firstname} !`;
 
-console.log(message)
+if(hour>=18) {
+    console.log(`Bonsoir ${firstname} !`);
+} else {
+    console.log(`Bonjour ${firstname} !`);
+}
+
+// console.log(message)
 };
 
-sayHello("Beyonce");
+sayHello("Beyonce", 11);
+sayHello("Beyonce", 18);
+sayHello("Beyonce", 17);
