81419fc25619ad7ee28e8f0b87c5e6d7149a1613
Elouan <elouangauriaud@gmail.com>
2025-06-12 14:24:15 +0200

    Etape 3

--- 2. Bonjour_JavaScript/script.js
+++ 2. Bonjour_JavaScript/script.js
@@ -1,5 +1,10 @@
-let firstname = "Beyonce";
+function sayHello(firstname) {
+
+// let firstname = "Beyonce";
 
 let message = `Bonjour ${firstname} !`;
 
 console.log(message)
+};
+
+sayHello("Beyonce");
